773015148b2150bcdadcd0b342a1b6b6e059a9bd
Steele Johnson <steelejohnson1986@gmail.com>
2025-12-06 15:36:41 +1100

    CH2 L4: FPS completed

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while 1:
         log_state()
         
@@ -20,6 +23,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        dt = game_clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
